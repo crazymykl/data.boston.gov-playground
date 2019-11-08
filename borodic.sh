fa08734693d25fb37a67a775a303e8ea803fe131
Chris Borodic <chris.borodic@pillpack.com>
2019-11-08 18:12:15 -0500

    Update borodic.sh

--- borodic.sh
+++ borodic.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e ".separator ","\n.import tmp5c00jrqx.csv test" | sqlite3 test.db && sqlite3 test.db "select year, district, CAST(offense_code as Integer) as offense_code, count(incident_number) from test where CAST(offense_code as Integer) IN (413, 3301, 802, 311) group by year, district, offense_code"
+echo -e ".separator ","\n.import tmp5c00jrqx.csv test" | sqlite3 test.db && sqlite3 test.db "select year, district, CAST(offense_code as Integer), count(distinct incident_number) from test where CAST(offense_code as Integer) IN (413, 3301, 802, 311) group by year, district, cast(offense_code as Integer)"
